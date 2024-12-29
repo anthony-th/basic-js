c99fa75ed8df4a4beb3e19e34d2d04918b53f005
anthony-th <yefremov.anthony@gmail.com>
2024-12-29 16:34:23 +0300

    feat: add VigenereCipheringMachine solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(directMode = true) {
+    this.directMode = directMode;
+    this.baseAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  transform(text, keyword, mode) {
+    if (!text || !keyword) {
+      throw new Error('Incorrect arguments!');
+    }
+    const formattedText = text.toUpperCase();
+    const formattedKey = keyword.toUpperCase();
+    const keySize = formattedKey.length;
+    let keyPointer = 0;
+    const transformed = Array.from(formattedText).map((symbol) => {
+      const textCharIndex = this.baseAlphabet.indexOf(symbol);
+      if (textCharIndex === -1) {
+        return symbol;
+      }
+      const keyCharIndex = this.baseAlphabet.indexOf(formattedKey[keyPointer % keySize]);
+      keyPointer++;
+      const newIndex = mode === 'encrypt' ? (textCharIndex + keyCharIndex) % 26 : (textCharIndex - keyCharIndex + 26) % 26;
+      return this.baseAlphabet[newIndex];
+    });
+    const finalResult = transformed.join('');
+    return this.directMode ? finalResult : finalResult.split('').reverse().join('');
+  }
+  encrypt(plainText, keyPhrase) {
+    return this.transform(plainText, keyPhrase, 'encrypt');
+  }
+  decrypt(cipherText, keyPhrase) {
+    return this.transform(cipherText, keyPhrase, 'decrypt');
   }
 }
 
